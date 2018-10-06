72be5e85958c1edfbf91547d06a33d569cf6fac5
tavishjain <jaintavish@gmail.com>
2018-10-06 12:25:43 +0530

    CHanges done

--- app/src/main/java/ytv/appathon/appathon_igdtu/HelpWidget.java
+++ app/src/main/java/ytv/appathon/appathon_igdtu/HelpWidget.java
@@ -1,8 +1,11 @@
 package ytv.appathon.appathon_igdtu;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
@@ -10,16 +13,35 @@ import android.widget.RemoteViews;
  */
 public class HelpWidget extends AppWidgetProvider {
 
+    private static final String MyOnClick = "myOnClickTag";
+
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.help_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setOnClickPendingIntent(R.id.appwidget_help_button, getPendingSelfIntent(context, MyOnClick));
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    public static PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, HelpWidget.class);
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+
+    public void onReceive(Context context, Intent intent) {
+
+        if (MyOnClick.equals(intent.getAction())){
+            Log.e("tavish", "widget clicked ");
+
+            
+
+            //TODO add here the method for siren and share location
+        }
     }
 
     @Override
