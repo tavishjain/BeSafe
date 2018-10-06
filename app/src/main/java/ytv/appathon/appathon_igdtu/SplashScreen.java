c4e33227fa0b2e5146663e1c1e2e8f87a7bc2694
Yugansh Tyagi <yugansh39@gmail.com>
2018-10-06 11:25:43 +0530

    Changed Splash Screen

--- app/src/main/java/ytv/appathon/appathon_igdtu/SplashScreen.java
+++ app/src/main/java/ytv/appathon/appathon_igdtu/SplashScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,10 +13,13 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
             }
         }, 2000);
     }
