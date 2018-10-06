73c9e69620f66d3cce90f7a31c594dca9d82d32b
Yugansh Tyagi <yugansh39@gmail.com>
2018-10-06 14:18:21 +0530

    Completed basic main app UI

--- app/src/main/java/ytv/appathon/appathon_igdtu/MainActivity.java
+++ app/src/main/java/ytv/appathon/appathon_igdtu/MainActivity.java
@@ -1,19 +1,46 @@
 package ytv.appathon.appathon_igdtu;
 
+import android.Manifest;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback, LocationListener {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.toolBarText)
     TextView toolbarText;
+
+    private GoogleMap googleMap;
+    private LocationManager locationManager;
+
+    private double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +49,79 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        init();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Dexter.withActivity(MainActivity.this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        try {
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                                    20000, 0,
+                                    MainActivity.this);
+                        } catch (SecurityException e) {
+                            e.printStackTrace();
+                        }
+                        init();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity.this,
+                                "Location Permission Required",
+                                Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
     }
 
     private void init() {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
         toolbarText.setText(getResources().getString(R.string.app_name));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        Log.d("Latitude is", String.valueOf(latitude));
+        Log.d("Longitude is", String.valueOf(longitude));
+
+        LatLng currentLoc = new LatLng(latitude, longitude);
+        googleMap.addMarker(new MarkerOptions().position(currentLoc));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
