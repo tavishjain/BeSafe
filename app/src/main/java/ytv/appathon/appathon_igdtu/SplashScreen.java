e059b422a3acddbef524dc9e230f545ae3f72eb7
Vishal Sehgal <codervishalsehgal@gmail.com>
2018-10-06 12:59:04 +0530

    Fixed Splash Screen

--- app/src/main/java/ytv/appathon/appathon_igdtu/SplashScreen.java
+++ app/src/main/java/ytv/appathon/appathon_igdtu/SplashScreen.java
@@ -22,6 +22,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(new Intent(SplashScreen.this, MainActivity.class));
                 finish();
             }
-        }, 700);
+        }, 1000);
     }
 }
