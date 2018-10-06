2273738120a1b4e845f6bdc64cfce392824908c0
tavishjain <jaintavish@gmail.com>
2018-10-06 14:46:35 +0530

    Widget fixure

--- app/src/main/java/ytv/appathon/appathon_igdtu/MainActivity.java
+++ app/src/main/java/ytv/appathon/appathon_igdtu/MainActivity.java
@@ -140,7 +140,7 @@ public class MainActivity extends AppCompatActivity
         new LovelyStandardDialog(this, LovelyStandardDialog.ButtonLayout.VERTICAL)
                 .setTopColorRes(R.color.colorPrimary)
                 .setButtonsColorRes(R.color.colorAccent)
-              //TODO add the app icon  .setIcon(R.drawable.ic_star_border_white_36dp)
+                .setIcon(R.drawable.app_logo_main_round)
                 .setTitle(safetyTip[0])
                 .setMessage(safetyTip[1])
                 .setPositiveButton("Hide Tips", new View.OnClickListener() {
